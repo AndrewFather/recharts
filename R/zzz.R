a1bd44d0bf8e6262f777df707b9ce37d691915e2
Yiying Wang <madlogos@users.noreply.github.com>
2016-07-28 12:07:47 +0800

    windows > Windows
    Sys.info()[['sysname']] == 'Windows'

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname='recharts'){
-    if (Sys.info()[['sysname']] == 'windows'){
+    if (Sys.info()[['sysname']] == 'Windows'){
         Sys.setlocale('LC_CTYPE', 'Chs')
     }else{
         Sys.setlocale('LC_CTYPE', 'zh_CN.utf-8')

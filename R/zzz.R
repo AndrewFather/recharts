13509956cff7b0224f9e164bbeabf41350f7e084
Yiying Wang <madlogos@users.noreply.github.com>
2016-08-04 18:10:05 +0800

    move validChartTypes outside .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
+validChartTypes <<- read.csv(
+        system.file('validChartTypes.csv', package = 'recharts'), header=TRUE)
+
 .onLoad <- function(libname, pkgname='recharts'){
     if (Sys.info()[['sysname']] == 'Windows'){
         Sys.setlocale('LC_CTYPE', 'Chs')
     }else{
         Sys.setlocale('LC_CTYPE', 'zh_CN.utf-8')
     }
-    validChartTypes <<- read.csv(
-        system.file('validChartTypes.csv', package = 'recharts'), header=TRUE)
-
 }

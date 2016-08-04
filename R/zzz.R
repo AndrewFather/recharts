ea8761694dfb5ad77d4d6064eff731d621b29462
Yiying Wang <madlogos@users.noreply.github.com>
2016-08-04 18:01:42 +0800

    load validChartTypes

--- R/zzz.R
+++ R/zzz.R
@@ -4,5 +4,7 @@
     }else{
         Sys.setlocale('LC_CTYPE', 'zh_CN.utf-8')
     }
-}
+    validChartTypes <<- read.csv(
+        system.file('validChartTypes.csv', package = 'recharts'), header=TRUE)
 
+}

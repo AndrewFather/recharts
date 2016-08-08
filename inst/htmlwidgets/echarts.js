f1a364ddca38511f67677bfa7fdf60c8eb8801f0
Yiying Wang <madlogos@users.noreply.github.com>
2016-08-08 18:28:18 +0800

    set theme

--- inst/htmlwidgets/echarts.js
+++ inst/htmlwidgets/echarts.js
@@ -2,15 +2,18 @@ HTMLWidgets.widget({
   name: 'echarts',
   type: 'output',
 
-  initialize: function(el, width, height) {
+  initialize: function(el, width, height) {		
     return echarts.init(el);
   },
 
-  renderValue: function(el, x, instance) {
-    instance.setOption(x);
+  renderValue: function(el, x, instance) {    	
+    var theme = x['theme'];
+	instance.setTheme(JSON.parse(theme));  
+	instance.setOption(x);
   },
 
   resize: function(el, width, height, instance) {
   }
-
+	
+  
 });

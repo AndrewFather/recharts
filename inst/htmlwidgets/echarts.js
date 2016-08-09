73829ca0cd1cea595135fdb06a7ffc90ff4a3de9
Yiying Wang <madlogos@users.noreply.github.com>
2016-08-09 18:22:27 +0800

    setTheme, not successful

--- inst/htmlwidgets/echarts.js
+++ inst/htmlwidgets/echarts.js
@@ -6,20 +6,29 @@ HTMLWidgets.widget({
     return echarts.init(el);
   },
 
+
   renderValue: function(el, x, instance) {
-    var theme = x.theme;
-    //require(['lib/themes/' + theme], function(theme){instance.setTheme(theme);});
-    if (theme  === "default" || theme === "macarons" || theme === "infographic"){
-       instance.setTheme(theme);
-    }else{
-        //var themeObj = eval(JSON.parser('lib/themes/' + theme + '.js'));
-        require('lib/themes/' + theme);
+    var theme = 'default';
+    if (typeof(x.theme) != 'undefined'){ theme = x.theme; }
+
+    if (theme === "default" || theme === "macarons" ||
+       theme === "infographic"){
         instance.setTheme(theme);
+    }else{
+
+        var themeObj = require(['../lib/echarts/echarts-all',
+           '../lib/echarts/themes/' + theme],
+           function(theme){ return theme;});
+        //var themeObj = JSON.parse(theme);
+
+        alert(themeObj);
+        instance.setTheme(themeObj);
     }
 	instance.setOption(x);
   },
 
   resize: function(el, width, height, instance) {
+
   }
 
 
